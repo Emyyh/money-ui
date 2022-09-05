575feb831333ef29e99c6492c87b6e55e01ca028
lehayashida <lehayashida@gmail.com>
2022-09-05 10:49:09 -0300

    button e input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { PrimengModule } from './primeng.module';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-rounting.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    PrimengModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
