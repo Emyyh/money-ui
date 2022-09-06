be40537340db89edf526f9a0989a428a0c07a917
lehayashida <lehayashida@gmail.com>
2022-09-06 15:39:38 -0300

    tela login

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { LoginComponent } from './pages/seguranca/login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
+import { LoginComponent } from './pages/seguranca/login/login.component';
+import { PrimengModule } from './primeng.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { PrimengModule } from './primeng.module';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-rounting.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent
   ],
   imports: [
-    AppRoutingModule,
-    BrowserModule,
     CommonModule,
+    BrowserModule,
     BrowserAnimationsModule,
-    PrimengModule,
-    FormsModule
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
