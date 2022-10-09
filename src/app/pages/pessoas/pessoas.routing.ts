a42c7b2a105b96f326eddd3c7bd9602358475f2a
lehayashida <lehayashida@gmail.com>
2022-10-08 21:51:27 -0300

    navbar

---
+++ src/app/pages/pessoas/pessoas.routing.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PessoaCadastroComponent } from "./pessoa-cadastro/pessoa-cadastro.component";
+import { PessoaListaComponent } from "./pessoa-lista/pessoa-lista.component";
+
+const routes: Routes = [
+  {path:'pessoas', component: PessoaListaComponent },
+  {path:'pessoas/novo', component: PessoaCadastroComponent },
+  {path:':codigo', component: PessoaCadastroComponent },
+
+];
+
+@NgModule({
+  imports:[
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class PessoasRouting {}
